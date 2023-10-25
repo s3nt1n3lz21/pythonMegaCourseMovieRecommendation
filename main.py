da1240734675924b4243b9e0cf89cf378502e583
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-10-25 16:04:32 +0100

    Now recommending movies by content and by comparing to similar peoples ratings

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-
+from surprise import Dataset, Reader, SVD, model_selection
 
 pandas.set_option('display.max_columns', None)
 
@@ -10,6 +10,14 @@ def get_weighted_rating(df, min_votes, average_rating_all_movies):
     num_votes = df["vote_count"]
     weighted_rating = (num_votes / (num_votes + min_votes)) * average_rating + (min_votes / (num_votes + min_votes)) * average_rating_all_movies
     return weighted_rating
+
+def similar_movies(similarity_matrix, movie_index):
+    scores = list(enumerate(similarity_matrix[movie_index]))
+    print(scores)
+    sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+    print(sorted_scores)
+    movies = [item[0] for item in sorted_scores[1:]]
+    return movies
 
 if __name__ == '__main__':
     # Popularity Based Filtering
@@ -28,14 +36,41 @@ if __name__ == '__main__':
     # print(movies_filtered.head(10))
 
     # Content Based Filtering
-    movies_small = pandas.read_csv("movies_small.csv", sep=";")
-    print(movies_small.head())
+    movies_small = pandas.read_csv("movies.csv")
+    # print(movies_small.head())
 
     tfidf = TfidfVectorizer(stop_words="english")
     movies_small["overview"] = movies_small["overview"].fillna("")
     tfidf_matrix = tfidf.fit_transform(movies_small["overview"].values.astype('U'))
 
-    print(pandas.DataFrame(tfidf_matrix.toarray(), columns=tfidf.get_feature_names_out()))
+    # print(pandas.DataFrame(tfidf_matrix.toarray(), columns=tfidf.get_feature_names_out()))
     similarity_matrix = linear_kernel(tfidf_matrix, tfidf_matrix)
     print(similarity_matrix)
+    movie_title = "John Carter"
+    movie_details = movies_small.loc[movies_small['title'] == movie_title]
+    movie_index = movie_details.index[0]
+    print(movie_index)
+    movies_index_similar_by_overview = similar_movies(similarity_matrix, movie_index)
+    print(movies_index_similar_by_overview)
+    # print(movies_small.iterrows())
+    movies_similar_by_overview = movies_small["title"].iloc[movies_index_similar_by_overview]
+    print(movies_similar_by_overview)
 
+    # Collaborative Filtering
+    # Predict what rating a user would give a movie they have not seen yet based on similar peoples ratings
+    print(ratings.head())
+    reader = Reader(rating_scale=(1, 5))
+    dataset = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
+
+    trainset = dataset.build_full_trainset()
+    svd = SVD()
+    svd.fit(trainset)
+
+    user_id = 15
+    movie_id = 1956
+    predicted_rating = svd.predict(user_id, movie_id)
+    print(predicted_rating.est)
+
+    # Validate the model, check the models prediction against the actual whole dataset
+    model_errors = model_selection.cross_validate(svd, dataset, measures=["RMSE", "MAE"])
+    print(model_errors)
